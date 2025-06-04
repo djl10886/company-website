dd50fc8d630d0d5b1f9f6b8912a33876652445c0
Dennis Li <djl10886@yahoo.com>
2025-06-04 11:31:58 -0400

    add support for direct links

--- README.md
+++
@@ -1,3 +0,0 @@
-# company-website
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/djl10886/company-website)
--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,16 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    middleware: [
+      (req, res, next) => {
+        if (req.url.includes('.')) return next();
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(fs.readFileSync('index.html'));
+      },
+    ],
+  },
+  preview: {
+    port: 5173,
+  },
 });
