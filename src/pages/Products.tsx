51310c81b5991190c0d8436c6f4d477222dad214
Dennis Li <djl10886@yahoo.com>
2026-01-11 20:09:55 -0500

    change embedded youtube link to fixed voiceover demo

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -40,7 +40,7 @@ export default function Products() {
         <div className="max-w-4xl mx-auto">
           <div className="relative pb-[56.25%] h-0 rounded-2xl overflow-hidden shadow-2xl">
             <iframe
-              src="https://www.youtube.com/embed/hbqNzIEnAYk"
+              src="https://www.youtube.com/embed/ANQRXpAMjt4"
               title="Product Demo"
               className="absolute top-0 left-0 w-full h-full"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
